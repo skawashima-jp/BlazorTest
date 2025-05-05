30a82ad4ae606cf97224ef1a60036484dd34e375
skawashima-jp <skawashima-jp@users.noreply.github.com>
2025-05-05 04:55:26 +0000

    deploy: 3a4917b026c2005712b0729047155a1bf7eb0b8c

---
+++ js/download.js
@@ -0,0 +1,8 @@
+﻿function triggerFileDownload(fileName, url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
